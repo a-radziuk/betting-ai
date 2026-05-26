e7c813ef1866511689afe1502b2d35314f0e352d
Alexey Radyuk <alexey.radyuk@gmail.com>
2026-05-26 18:03:59 +0300

    russian seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            RussianTournamentTranslationsSeeder::class,
+            RussianTeamTranslationsSeeder::class,
         ]);
     }
 }
