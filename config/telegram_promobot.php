c4488e8b1dac9620b71daddfc19293cedd031f7a
Alexey Radyuk <alexey.radyuk@gmail.com>
2026-06-27 17:12:29 +0300

    config

--- config/telegram_promobot.php
+++ config/telegram_promobot.php
@@ -8,6 +8,6 @@ return [
 
     'token' => env('TELEGRAM_PROMOBOT_TOKEN'),
 
-    'partner_codes' => ['55502', '55501', '55503', '55504'],
+    'partner_codes' => ['44403', '55501', '11108', '22207'],
 
 ];
